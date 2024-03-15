59f428ff7e7c919e45bebcd0f0fe81dc0e317c63
clair-de-lume <luisa_menezes@usp.br>
2024-03-15 13:23:00 -0300

    add sand

--- ep2/pescaria.js
+++ ep2/pescaria.js
@@ -12,6 +12,7 @@ window.onload = main;
 */
 var ctx;
 const AREIA_ALT = 0.30;
+const canvas = document.getElementById("meucanvas");
 
 /* ==================================================================
     função main
@@ -28,31 +29,51 @@ function construaInterface(){
 
   constroiCanvas();
 
+  
   // registro das funções de callback;
   window.onresize = callbackResize;
 }
 
 /**
  * Inicializa o canvas na variável global ctx
- */
+*/
 function constroiCanvas() {
-  const canvas = document.getElementById("meucanvas");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-
+  
   ctx = canvas.getContext('2d');
   console.log("construiu o canva!")
   if (!ctx) {
     alert("Não consegui abrir o contexto 2d!");
   }
+  desenhaAreia();
+
 }
 
+/**
+ * resize o tamanho do canvas e da areia ao mudar
+ * o tamanho da tela
+*/
 function callbackResize() {
-  console.log("resizou!");
+  console.log("resizou para resolucao", window.innerWidth, "x", window.innerHeight);
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
+  desenhaAreia();
+}
 
-  ctx.fillRect(30, 30, ctx.canvas.innerWidth, ctx.canvas.innerHeight);
-  ctx.fillStyle = 'yellow';
-  ctx.fillRect(ctx.canvas.innerWidth - AREIA_ALT, ctx.canvas.innerHeight - AREIA_ALT, ctx.canvas.innerWidth, AREIA_ALT);
+/**
+ * desenha a areia de acordo com a proporção
+ * da altura do canvas
+*/
+function desenhaAreia() {
+  console.log("desenhou areia");
+  let alturaAreia = AREIA_ALT * window.innerHeight
+  ctx.fillStyle =  'yellow';
+  ctx.fillRect(0, window.innerHeight - alturaAreia, window.innerWidth, alturaAreia);
+
+}
+
+function desenhaArpao() {
+  console.log("desenhou arpão");
+  
 }
