78f093d92276a901454aff96319cc2fc826dd967
clair-de-lume <luisa_menezes@usp.br>
2024-04-05 16:52:52 -0300

    finish (kind of)

--- ep2/pescaria.js
+++ ep2/pescaria.js
@@ -15,19 +15,26 @@ window.onload = main;
     constantes e variáveis globais
 */
 var gInterface = {
-  start: "Jogar",
+  pausado: true,
 }
 var ctx;
 const AREIA_ALT = 0.30;
 const canvas = document.getElementById("meucanvas");
 const JANELA_W = window.innerWidth;
 const JANELA_H = window.innerHeight;
-const N_PEIXES = 1;
+
+const ESQUERDA = 0;
+const DIREITA = 1;
 
 var MAR_W;
 var MAR_H;
+var N_PEIXES = 10;
+var VELOCIDADE_JOGO = 10;
 
 var peixes = [];
+var arpao;
+var BOLHA_ATIVA = 0;
+var bolha;
 
 class Poligono {
   raio;
@@ -58,13 +65,11 @@ class Poligono {
     for (let i = 0; i < this.lados; i++) {
       let angulo = (2 * Math.PI * i) / this.lados;
       this.vertices.push([this.x + this.raio * Math.cos(angulo), this.y + this.raio * Math.sin(angulo)]);
-      //console.log("vertices: ", this.vertices);
     }
   }
 
   desenhePoligono() {
     let poli = new Path2D();
-    ctx.clearRect(0, 0, MAR_W, MAR_H);
     ctx.beginPath();
     poli.moveTo( this.vertices[0][0], this.vertices[0][1] );
     for (let i = 1; i < this.vertices.length; i++) {
@@ -111,64 +116,211 @@ class Peixe extends Poligono {
     this.nVertices();
     this.desenhePoligono();
   }
+
+  movePeixe() {
+    this.x = this.x + this.velX * (VELOCIDADE_JOGO/10);
+    this.y = this.y + this.velY * (VELOCIDADE_JOGO/10);
+    
+    if (this.x < this.raio) {
+      this.velX *= -1;
+    }
+    if (this.x >= MAR_W - this.raio) {
+      this.velX *= -1;
+    }
+    if (this.y < this.raio) {
+      this.velY *= -1;
+    }
+    if (this.y >= MAR_H - this.raio) {
+      this.velY *= -1;
+    }
+  }
+}
+
+class Arpao {
+  a;
+  b;
+  c;
+  cor;
+
+  constructor() {
+    this.a = [JANELA_W/2, JANELA_H - (JANELA_H*AREIA_ALT) + 40];
+    this.b = [JANELA_W/2 - 50, JANELA_H - 40];
+    this.c = [JANELA_W/2 + 50, JANELA_H - 40];
+
+    this.cor = "brown";
+  }
+
+  desenhaArpao() {
+    let poli = new Path2D();
+    ctx.beginPath();
+    poli.moveTo(this.a[0], this.a[1]);
+    poli.lineTo(this.b[0], this.b[1]);
+    poli.lineTo(this.c[0], this.c[1]);
+    poli.closePath(); 
+  
+    ctx.fillStyle = this.cor;
+    ctx.fill(poli);
+  }
+  
+  moveArpao(direcao) {
+    if (direcao == ESQUERDA) {
+      this.a[0] -= 10;
+      this.b[0] -= 10;
+      this.c[0] -= 10;
+    }
+    else {
+      this.a[0] += 10;
+      this.b[0] += 10;
+      this.c[0] += 10;
+    }
+  }
+}
+
+class Bolha extends Poligono {
+  velY = 5;
+
+  constructor(x, y) {
+    super();
+
+    this.cor = "blue";
+    this.lados = 64;
+    this.raio = 10
+
+    this.x = x,
+    this.y = y;
+  }
+
+  moveBolha() {
+    if (BOLHA_ATIVA) {
+      this.y = this.y - this.velY * VELOCIDADE_JOGO/10;
+    }
+    if (bolha.y < 0) {
+      BOLHA_ATIVA = 0;
+    }
+  }
+
+  desenhaBolha() {
+    this.nVertices();
+    this.desenhePoligono();
+  }
+
+  checaColisao(peixe) {
+    let d = distancia(peixe.x, peixe.y, bolha.x, bolha.y);
+    if (d <= peixe.raio + bolha.raio / 2) {
+      BOLHA_ATIVA = 0;
+      return true;
+    }
+  }
 }
 
 /* ==================================================================
     função main
 */
 function main() {
-  construaInterface();
-
-  criaPeixes();
-  
-  console.log("xo, yo", peixes[0].x, peixes[0].y);
-  console.log("velX velY", peixes[0].velX, peixes[0].velY)
-  console.log("vertices", peixes[0].vertices);
-  
-  redesenhe();
-  
-  //console.log("xo, yo", peixes[0].x, peixes[0].y);
-  //console.log("vertices", peixes[0].vertices);
-}
-
-/**
- * Registra os elementos HTML responsáveis pela interação no objeto
- * interface e os associa às rotinas de callback.
- */
-function construaInterface(){
-
-  constroiCanvas();
-  
-
-  // registro das funções de callback;
-  window.onresize = callbackResize;
-}
-
-/**
- * Inicializa o canvas na variável global ctx
-*/
-function constroiCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   
   ctx = canvas.getContext('2d');
-  console.log("construiu o canva!")
   if (!ctx) {
     alert("Não consegui abrir o contexto 2d!");
   }
-  desenhaAreia();
+
+  // redesenha elementos pela primeira vez
+  
+  gInterface.start = document.getElementById("btJogar");
+  gInterface.passo = document.getElementById("btPasso");
+  gInterface.velocidade = document.getElementById("rgVelocidade");
+  gInterface.pVelocidade = document.getElementById("pVelocidade");
+  
+  // registro das funções de callback;
+  window.onresize = callbackResize;
+  window.onkeydown = callbackKeyDown;
+  gInterface.start.onclick = callbackJogar;
+  gInterface.passo.onclick = callbackPasso;
+  gInterface.velocidade.onclick = callbackVelocidade;
+
+  callbackResize();
+  
+  criaPeixes();
+  criaArpao();
 
 }
 
 /**
- * resize o tamanho do canvas e da areia ao mudar
+ * resize o tamanho dos elementoss do jogo ao mudar
  * o tamanho da tela
 */
 function callbackResize() {
-  console.log("resizou para resolucao", window.innerWidth, "x", window.innerHeight);
+  // resize mar
   ctx.canvas.width = window.innerWidth;
   ctx.canvas.height = window.innerHeight;
+
+  // resize areia
   desenhaAreia();
+
+  // resize peixes
+  //for(let i = 0; i < N_PEIXES; i++) {
+    //peixes[i].desenhaPeixe();
+  //}
+
+  // resize arpao
+  //arpao.desenhaArpao();
+
+  // resize bolha
+  //
+}
+
+function callbackKeyDown(e) {
+  if (!gInterface.pausado) {
+    let tecla = e.keyCode;
+    // tecla A: arpao move para esquerda
+    if (tecla == 65) {
+      desenhaAreia();
+      arpao.moveArpao(ESQUERDA);
+      arpao.desenhaArpao();
+    }
+  
+    // tecla D: arpao move para direita
+    else if (tecla == 68) {
+      desenhaAreia();
+      arpao.moveArpao(DIREITA);
+      arpao.desenhaArpao();
+    }
+  
+    // tecla S: arpao dispara bolha
+    else if (tecla == 83) {
+      if (!BOLHA_ATIVA) {
+        BOLHA_ATIVA = 1;
+        bolha = new Bolha(arpao.a[0], arpao.a[1]);
+      }
+    }
+  }
+}
+
+function callbackJogar() {
+  let u = gInterface.start.value;
+  if (u == "Jogar") {
+    gInterface.start.value = "Pausar";
+    gInterface.pausado = false;
+    gInterface.passo.disabled = true;
+    jogar(); 
+  }
+  else {
+    gInterface.start.value = "Jogar";
+    gInterface.pausado = true;
+    gInterface.passo.disabled = false;
+  }
+}
+
+function callbackPasso() {
+  if(gInterface.start.value == "Jogar") {
+    passo();
+  }
+}
+
+function callbackVelocidade() {
+  VELOCIDADE_JOGO = gInterface.velocidade.value;
+  gInterface.pVelocidade.innerHTML = "Velocidade x" + VELOCIDADE_JOGO/10;
 }
 
 /**
@@ -176,31 +328,28 @@ function callbackResize() {
  * da altura do canvas
 */
 function desenhaAreia() {
-  //console.log("desenhou areia");
   let alturaAreia = AREIA_ALT * window.innerHeight
   ctx.fillStyle =  'yellow';
   ctx.fillRect(0, window.innerHeight - alturaAreia, window.innerWidth, alturaAreia);
 
   MAR_H = window.innerHeight - alturaAreia;
   MAR_W = JANELA_W;
-  //console.log("altura areia: ", alturaAreia)
-  //console.log("altura mar: ", MAR_H);
-}
-
-function desenhaArpao() {
-  console.log("desenhou arpão");
-  
 }
 
 /**
- * cria um array com n peixes e os desenha pela primeira vez
+ * cria um array com n peixes
  */
 function criaPeixes() {
   for(let i = 0; i < N_PEIXES; i++) {
     let peixe = new Peixe();
     peixes.push(peixe);
-    //peixes[i].desenhaPeixe();
+    peixe.desenhaPeixe();
   }
+}
+
+function criaArpao() {
+  arpao = new Arpao();
+  arpao.desenhaArpao();
 }
 
 /**
@@ -211,49 +360,57 @@ function sorteie_inteiro(min, max) {
 }
 
 /**
- * dado um peixe com raio r, centro C = (x, y) e velocidades
- * velX e velY, calcule a nova posicao do peixe a cada frame
+ * calcula a distancia entre dois pontos (x1, y1) e (x2, y2)
  */
-function redesenhe() {
-  passo();
-  // requisita o próximo redesenho 
-  requestAnimationFrame(redesenhe);
+function distancia(x1, y1, x2, y2){
+  return (Math.sqrt((x1-x2)**2 + (y1-y2)**2))
 }
 
+/**
+ * dado uma lista de peixes, redesenha cada um deles
+ * em sua nova posicao continuamente
+ * 
+ * Usar esse para o botao Jogar
+ */
+function jogar() {
+  if (!gInterface.pausado) {
+    passo();
+    requestAnimationFrame(jogar);
+  }
+}
+
+/**
+ * dado uma lista de peixes, redesenha cada um deles
+ * em sua nova posicao 1 frame por vez
+ * 
+ * Usar esse para o botao Passo
+ */
 function passo() {
-  ctx.clearRect(0, 0, MAR_W, MAR_H);
+  // limpa mar
+  ctx.fillStyle = "darkcyan";
+  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
   for(let i = 0; i < N_PEIXES; i++) {
     let peixe = peixes[i];
-    // atualiza a cena, ou seja, a posição do objeto
-    peixe.x = peixe.x + peixe.velX;
-    peixe.y = peixe.y + peixe.velY;
-  
-    
-    if (peixe.x < 0) {
-      peixe.velX *= -1;
-      peixe.x = -peixe.x;
+    peixe.movePeixe();
+    peixe.desenhaPeixe();
+  }
+
+  if (BOLHA_ATIVA) {
+    bolha.moveBolha();
+    for(let i = 0; i < N_PEIXES; i++) {
+      if (bolha.checaColisao(peixes[i])) {
+        peixes.splice(i, 1);
+        N_PEIXES -= 1;
+        console.log(peixes);
+      }
     }
-    if (peixe.x >= MAR_W) {
-      peixe.velX *= -1;
-      peixe.x = MAR_W - (peixe.x - MAR_W);
-    }
-    if (peixe.y < 0) {
-      peixe.velY *= -1;
-      peixe.y = -peixe.y;
-    }
-    if (peixe.y >= MAR_H) {
-      peixe.velY *= -1;
-      peixe.y = MAR_H - (peixe.y - MAR_H);
-    }
-  
-    // ciclo da animação: limpa a tela e desenhe
-    //ctx.fillStyle = 'darkcyan';
-    //ctx.clearRect(0, 0, MAR_W, MAR_H);
-    desenhaAreia();
-  
-    // desenha o peixe na nova posicao
-    peixe.nVertices();
-    peixe.desenhePoligono();
+  }
+
+  desenhaAreia();
+  arpao.desenhaArpao();
+
+  if (BOLHA_ATIVA) {
+    bolha.desenhaBolha();
   }
 }
--- ep2/to-do.md
+++ ep2/to-do.md
@@ -1,13 +1,20 @@
 # To-do
 - OK adicionar canvas responsivo
 - OK adicionar areia
-- adicionar arpão
-- adicionar peixes
-- adicionar titulo da página e botões
+- OK adicionar arpão
+- OK adicionar peixes
+- OK adicionar titulo da página e botões
+- OK adicionar bolha e trajetoria reta
+- OK adicionar check para bolha encostar em peixe
+- normalizar canvas
+- OK adicionar range para velocidade
+- redesenhar canva ao dar resize
+- OK adicionar "x0.5 velocidade, x 1.5 velocidade" ao lado do range (adicional)
 
 # Fixes
-- atualmente, peixe é refletido apenas quando o CENTRO do peixe encosta nas bordas, mas deve ser refletido assim que qualquer parte do peixe encoste numa borda
-- peixe nao esta apagando a posição antiga e está criando uma linha contínua
+- OK atualmente, peixe é refletido apenas quando o CENTRO do peixe encosta nas bordas, mas deve ser refletido assim que qualquer parte do peixe encoste numa borda
+- OK peixe nao esta apagando a posição antiga e está criando uma linha contínua
+- OK checar se bolha ja terminou caminho para poder disparar outra
 
 # Dúvidas
 - é pra areia ficar tão grossa assim? 
